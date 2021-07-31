8aced1ead426b9cc2338c843b9daafd2dd42e7e3
HongJungKim <hong267@naver.com>
2021-07-31 19:14:55 +0900

    Add baekjoon 0731_2

--- baekjoon/2559.py
+++ baekjoon/2559.py
@@ -1,31 +1,15 @@
-# 1번째 방법
 import sys
 
 N, K = map(int, sys.stdin.readline().split())
 temperatures = list(map(int, sys.stdin.readline().split()))
 
-if K == 1:
-    print(max(temperatures))
-else:
-    result_list = []
-    for i in range(N-K+1):
-        result_list.append(sum(temperatures[i:K+i]))
+sum_list = [sum(temperatures[:K])]
+tmp = sum_list[0]
 
-    print(max(result_list))
-
-
-# 2번째 방법
-import sys
-
-N, K = map(int, sys.stdin.readline().split())
-temperatures = list(map(int, sys.stdin.readline().split()))
-
-result_list = []
-i = 0
-for i in range(N-K+1):
-    tmp = 0
-    for j in range(K):
-        tmp += temperatures[i+j]
-    result_list.append(tmp)
-
-print(max(result_list))
+index_f, index_b = 0, K
+for i in range(1, N-K+1):
+    tmp = tmp - temperatures[index_f] + temperatures[index_b]
+    index_f += 1
+    index_b += 1
+    sum_list.append(tmp)
+print(max(sum_list))
