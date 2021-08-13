0bb306c107f294dc297e12a0134277e15d40429b
HongJungKim <hong267@naver.com>
2021-08-13 14:22:31 +0900

    Add baekjoon 0813_1

--- baekjoon/1966.py
+++ baekjoon/1966.py
@@ -7,7 +7,7 @@ for _ in range(T):
     N, M = map(int, sys.stdin.readline().split())
     numbers = list(map(int, sys.stdin.readline().split()))
 
-    target = str(numbers[M]) # 찾고자 하는 인덱스의 숫자를 문자형으로 따로 저장([1, 1, 9, 1, 1, 1] 같은 경우 찾고자 하는 숫자를 구별할 수 없으므로!)
+    target = str(numbers[M]) # 찾고자 하는 인덱스의 숫자를 문자형으로 따로 저장(['1', 1, 9, 1, 1, 1] 같은 경우 찾고자 하는 숫자를 구별할 수 없으므로!)
 
     deque1 = deque(numbers[:])
     deque1[M] = target # deque의 찾고자 하는 인덱스의 숫자도 문자형으로 바꿔줌
