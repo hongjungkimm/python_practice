20b464db025bc6e337492c745c70c232b25be4af
HongJungKim <hong267@naver.com>
2021-08-16 13:33:42 +0900

    Add baekjoon 0816_2

--- baekjoon/1208.py
+++ baekjoon/1208.py
@@ -1,14 +1,12 @@
-# import sys 시간초과
+# import sys
 
 # N, S = map(int, sys.stdin.readline().split())
 # numbers = list(map(int, sys.stdin.readline().split()))
 
-# n = len(numbers)
-
 # cnt = 0
-# for i in range(1, 1 << n):
+# for i in range(1, 1 << N):
 #     tmp = []
-#     for j in range(n):
+#     for j in range(N):
 #         if i & (1 << j):
 #             tmp.append(numbers[j])
 #     if sum(tmp) == S:
@@ -17,16 +15,14 @@
 # print(cnt)
 
 
-# import sys 시간초과
+# import sys
 # from itertools import combinations
 
 # N, S = map(int, sys.stdin.readline().split())
 # numbers = list(map(int, sys.stdin.readline().split()))
 
-# n = len(numbers)
-
 # cnt = 0
-# for i in range(n+1):
+# for i in range(N+1):
 #     for j in combinations(numbers, i):
 #         if j != () and sum(j) == S:
 #             cnt += 1
