3e0ea2fb8dc5ffdec3cf25c4afe83d21b07ba81d
HongJungKim <hong267@naver.com>
2021-09-18 15:19:47 +0900

    Add baekjoon 0918_2

--- baekjoon/20922.py
+++ baekjoon/20922.py
@@ -1,25 +1,22 @@
-import sys # 시간초과
+import sys
 
 N, K = map(int, sys.stdin.readline().split())
 numbers = list(map(int, sys.stdin.readline().split()))
 
 num_dict1 = {k:0 for k in numbers}
 result = 0
-length = 0
 i = 0
 j = 0
 while i < N:
     num_dict1[numbers[i]] += 1
-    length += 1
     if num_dict1[numbers[i]] > K:
-        if result < length - 1:
-            result = length - 1
-        num_dict1 = {k:0 for k in numbers}
-        length = 0
-        j += 1
-        i = j - 1
+        if result < i - j:
+            result = i - j
+        while num_dict1[numbers[i]] > K:
+            num_dict1[numbers[j]] -= 1
+            j += 1
     i += 1
-if result < length:
-    result = length
 
+if i - j > result:
+    result = i - j
 print(result)
