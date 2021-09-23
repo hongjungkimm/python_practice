38a559ae302dca40a2e241c248c7215eeaf74997
HongJungKim <hong267@naver.com>
2021-09-23 18:11:41 +0900

    Add SWEA 0923_1

---
+++ SWEA/5178.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    N, M, L = map(int, input().split())
+    nodes = [list(map(int, input().split())) for _ in range(M)]
+    tree = [[0] * 2 for _ in range(N+1)]
+
+    for node in nodes:
+        tree[node[0]][1] = node[1] # 리프노드 표시
+        tree[node[0]][0] = node[0] // 2 # 각 리프노드의 부모노드 표시
+
+    for node in tree[::-1]:
+        tree[node[0]][1] += node[1]
+        tree[node[0]][0] = node[0] // 2
+
+    print('#{0} {1}'.format(tc, tree[L][1]))
