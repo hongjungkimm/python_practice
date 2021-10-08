2428512e21001d5380598aa7a16fedca6bb8d8f2
HongJungKim <hong267@naver.com>
2021-10-08 10:20:55 +0900

    Add boj 1008_1

--- baekjoon/1068.py
+++ baekjoon/1068.py
@@ -1,12 +1,58 @@
 import sys
 
 def del_node(n):
-    return n
+    global nodes
+    nodes[n] = -2
+    for i in range(N):
+        if nodes[i] == n:
+            del_node(i)
 
 N = int(sys.stdin.readline())
 nodes = list(map(int, sys.stdin.readline().split()))
 num = int(sys.stdin.readline())
 
-tree = [[] for _ in range(51)]
-
 del_node(num)
+
+cnt = 0
+for i in range(len(nodes)):
+    if nodes[i] != -2 and i not in nodes:
+        cnt += 1
+
+print(cnt)
+
+
+# for node in nodes:
+#     if node != -2 and 
+# import sys
+
+# def del_node(n):
+#     global tree
+#     if len(tree[n]) <= 1:
+#         tree[n][0] = -2
+#         return
+
+#     for i in range(1, len(tree[n])):
+#         del_node(tree[n][i])
+
+
+# N = int(sys.stdin.readline())
+# nodes = list(map(int, sys.stdin.readline().split()))
+# num = int(sys.stdin.readline())
+
+# tree = [[] for _ in range(51)]
+
+# for i in range(N):
+#     if nodes[i] == -1:
+#         tree[i].append(nodes[i])
+#     else:
+#         tree[i].append(nodes[i])
+#         tree[nodes[i]].append(i)
+
+# del_node(num)
+
+# cnt = 0
+# for t in tree:
+#     if len(t) == 1 and t[0] != -2:
+#         cnt += 1
+
+# print(cnt)
