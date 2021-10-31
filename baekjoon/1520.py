d142487a92a3610292c672b1b0651baad2bca1e1
HongJungKim <hong267@naver.com>
2021-10-31 18:56:36 +0900

    Add boj 1031_1

--- baekjoon/1520.py
+++ baekjoon/1520.py
@@ -1,24 +1,27 @@
 import sys
+sys.setrecursionlimit(10000)
 
 def dfs(i, j):
-    di = [1, 0, 0, -1]
-    dj = [0, -1, 1, 0]
-    stack = [[i, j]]
-    cnt = 0
-    while stack:
-        x, y = stack.pop()
+    global visited
+    
+    if visited[i][j] == -1:
+        visited[i][j] = 0
         for d in range(4):
-            ni = x + di[d]
-            nj = y + dj[d]
+            ni = i + di[d]
+            nj = j + dj[d]
             if 0 <= ni < M and 0 <= nj < N:
-                if ni == M - 1 and nj == N - 1:
-                    cnt += 1
-                elif area[x][y] > area[ni][nj]:
-                    stack.append([ni, nj])
-    return cnt
+                if area[i][j] > area[ni][nj]:
+                    visited[i][j] += dfs(ni, nj)
+
+    return visited[i][j]
     
 
 M, N = map(int, sys.stdin.readline().split())
 area = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
+
+di = [1, 0, 0, -1]
+dj = [0, 1, -1, 0]
+visited = [[-1] * N for _ in range(M)]
+visited[M-1][N-1] = 1
 
 print(dfs(0, 0))
