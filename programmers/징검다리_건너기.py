df7cc03027bbe3b1bf530a6944946ecdecd5c902
kimhongjung <hong267@naver.com>
2022-01-31 13:42:52 +0900

    add programmers 0131_2

--- programmers/징검다리_건너기.py
+++ programmers/징검다리_건너기.py
@@ -1,50 +1,32 @@
 def solution(stones, k):
-    answer = 0
     length = len(stones)
-    zero_stones = [0] * length
-    while True:
+    left = 1
+    right = 200000000
+    while left <= right:
+        mid = (left + right) // 2
+        stones_copy = stones[:]
         for i in range(length):
-            if stones[i] > 0:
-                stones[i] -= 1
+            if stones_copy[i] - mid <= 0:
+                stones_copy[i] = 0
             else:
-                if zero_stones[i] == 0:
-                    zero_stones[i] = 1
-                for j in range(length - k + 1):
-                    if sum(zero_stones[j:j+k]) == k:
-                        return answer
+                stones_copy[i] -= mid
+        
+        check = 0
+        check_list = []
+        for i in range(length):
+            if stones_copy[i] == 0:
+                check += 1
+            else:
+                if check not in check_list:
+                    check_list.append(check)
+                check = 0
+        if check not in check_list:
+            check_list.append(check)
+
+        if max(check_list) >= k:
+            right = mid - 1
         else:
-            answer += 1
-    return answer
+            left = mid + 1
+    return left
 
 print(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3))
-
-# def solution(stones, k):
-#     length = len(stones)
-#     min_sum = float('inf')
-#     for i in range(length - k + 1):
-#         if sum(stones[i:i+k]) < min_sum:
-#             min_sum = sum(stones[i:i+k])
-    
-#     answer = float('inf')
-#     for i in range(length - k + 1):
-#         if sum(stones[i:i+k]) == min_sum and max(stones[i:i+k]) < answer:
-#             answer = max(stones[i:i+k])
-            
-#     return answer
-
-
-# def solution(stones, k):
-#     length = len(stones)
-#     zero_stones = []
-#     answer = 0
-#     while True:
-#         for i in range(length):
-#             if stones[i] > 0:
-#                 stones[i] -= 1
-#                 if stones[i] == 0 and i < length - k:
-#                     zero_stones.append(i)
-#         answer += 1
-#         for i in zero_stones:
-#             if sum(stones[i:i+k]) == 0:
-#                 return answer
-#     return answer
