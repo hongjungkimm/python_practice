a955cd63c07df54626e3e49c42b285348eb38dfe
kimhongjung <hong267@naver.com>
2022-02-02 18:53:30 +0900

    add programmers 0202_1

--- programmers/110_옮기기.py
+++ programmers/110_옮기기.py
@@ -1,32 +1,28 @@
 def solution(s):
     answer = []
     for i in s:
-        s_list = []
-        for j in i:
-            s_list.append(j)
         if len(i) <= 3:
             answer.append(i)
         else:
-            while True:
-                zero = 0
-                cnt = 0
-                for j in range(len(s_list)):
-                    if s_list[j] == '1':
-                        cnt += 1
-                    else:
-                        if cnt >= 3:
-                            zero = j
-                            s_list.pop(j)
-                            break
-                        cnt = 0
-                if zero:
-                    s_list.insert(j - 1, '0')
+            stack = []
+            cnt_110 = 0
+            for j in i:
+                if len(stack) >= 2 and stack[-1] == '1' and stack[-2] == '1' and j == '0':
+                    for _ in range(2):
+                        stack.pop()
+                    cnt_110 += 1
                 else:
-                    break
-            s_str = ''
-            for j in s_list:
-                s_str += j
-            answer.append(s_str)
+                    stack.append(j)
+            if cnt_110:
+                cnt_1 = 0
+                for s in stack[::-1]:
+                    if s == '0':
+                        break
+                    else:
+                        cnt_1 += 1
+                answer.append(''.join(stack[:len(stack) - cnt_1]) + '110' * cnt_110 + '1' * cnt_1)
+            else:
+                answer.append(i)
     return answer
 
-print(solution(["1110","100111100","0111111010", "11100"]))
+print(solution(["1110","100111100","0111111010", "11100", "101010110101010", "1000011110", "11110111101"]))
