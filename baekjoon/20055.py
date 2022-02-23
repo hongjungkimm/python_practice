7e6aaf49ba8e33c3ccb81b8fc00f0a6020934d92
kimhongjung <hong267@naver.com>
2022-02-23 23:39:10 +0900

    add boj 0223_2

--- baekjoon/20055.py
+++ baekjoon/20055.py
@@ -6,25 +6,27 @@ robot = [False] * N
 
 cnt = 0
 while True:
-    for _ in range(N * 2):
-        belt.insert(0, belt.pop())
+    cnt += 1
+    belt.insert(0, belt.pop())
+    robot.pop()
+    robot.insert(0, False)
+    if robot[-1] == True:
+        robot[-1] = False
     if robot.count(True):
         location = []
         for i in range(N):
             if robot[i] == True:
                 location.append(i)
-        for l in location:
-            if l == N - 1:
+        for l in location[::-1]:
+            if belt[l+1] and robot[l+1] == False:
+                belt[l+1] -= 1
+                robot[l+1] = True
                 robot[l] = False
-            else:
-                if belt[l+1] and robot[l+1] == False:
-                    belt[l+1] -= 1
-                    robot[l+1] = True
-                    robot[l] = False
+                if l + 1 == N - 1:
+                    robot[l+1] = False
     if belt[0]:
         robot[0] = True
-        belt[0] -= 1
-        cnt += 1
+        belt[0] -= 1  
     if belt.count(0) >= K:
         break
 
