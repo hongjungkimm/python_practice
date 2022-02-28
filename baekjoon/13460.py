180c906b10da820ee4e568fd9fb84b0d76138b32
kimhongjung <hong267@naver.com>
2022-02-28 15:06:15 +0900

    add boj 0228_1

--- baekjoon/13460.py
+++ baekjoon/13460.py
@@ -1,31 +1,58 @@
 import sys
+from collections import deque
 
-N, M = map(int, sys.stdin.readline().split())
+def init():
+    rx, ry, bx, by = 0, 0, 0, 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 'R':
+                rx, ry = i, j
+            elif graph[i][j] == 'B':
+                bx, by = i, j
 
-maze = []
-for _ in range(N):
-    line = sys.stdin.readline().rstrip()
-    tmp = []
-    for l in line:
-        tmp.append(l)
-    maze.append(tmp)
+    queue.append((rx, ry, bx, by, 1))
+    check[rx][ry][bx][by] = True
 
-red = []
-blue = []
-hole = []
-flag = False
-for i in range(N):
-    for j in range(M):
-        if maze[i][j] == 'R':
-            red = [i, j]
-        elif maze[i][j] == 'B':
-            blue = [i, j]
-        elif maze[i][j] == 'O':
-            hole = [i, j]
-        if red and blue and hole:
-            flag = True
+def move(x, y, dx, dy):
+    cnt = 0
+    while graph[x+dx][y+dy] != "#" and graph[x][y] != 'O':
+        x += dx
+        y += dy
+        cnt += 1
+    return x, y, cnt
+
+def bfs():
+    init()
+    while queue:
+        rx, ry, bx, by, depth = queue.popleft()
+        if depth > 10:
             break
-    if flag:
-        break
+        for d in range(4):
+            nrx, nry, rcnt = move(rx, ry, di[d], dj[d])
+            nbx, nby, bcnt = move(bx, by, di[d], dj[d])
+            if graph[nbx][nby] != 'O':
+                if graph[nrx][nry] == 'O':
+                    print(depth)
+                    return
+                if nrx == nbx and nry == nby:
+                    if rcnt > bcnt:
+                        nrx -= di[d]
+                        nry -= dj[d]
+                    else:
+                        nbx -= di[d]
+                        nby -= dj[d]
 
-answer = float('inf')
+                if not check[nrx][nry][nbx][nby]:
+                    check[nrx][nry][nbx][nby] = True
+                    queue.append((nrx, nry, nbx, nby, depth + 1)) 
+    print(-1)
+
+
+n, m = map(int, sys.stdin.readline().split())
+graph = [list(sys.stdin.readline().rstrip()) for _ in range(n)]
+di = [-1, 1, 0, 0]
+dj = [0, 0, -1, 1]
+queue = deque()
+check = [[[[False] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
+
+bfs()
