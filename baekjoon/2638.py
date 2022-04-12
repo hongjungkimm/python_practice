49c42c06355b0a75a40f4d088c93c2dfdab81dad
HongJungKim <hong267@naver.com>
2022-04-12 19:41:17 +0900

    add programmers 0412_1

---
+++ baekjoon/2638.py
@@ -0,0 +1,15 @@
+import sys
+
+def make_cheeze(cheeze):
+    di = [-1, 1, 0, 0]
+    dj = [0, 0, -1, 1]
+    pass
+
+def melt(cheeze):
+    pass
+
+def remove_air(cheeze):
+    pass
+
+N, M = map(int, sys.stdin.readline().split())
+cheeze = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
--- programmers/방금그곡.py
+++ programmers/방금그곡.py
@@ -33,6 +33,7 @@ print(solution("ABCDEFG", ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDE
 print(solution("CC#BCC#BCC#BCC#B", ["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]))
 print(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
 
+
 # from collections import defaultdict
 
 # def solution(m, musicinfos):
