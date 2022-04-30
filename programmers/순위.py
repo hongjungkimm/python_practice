cbbff7ffb55674c95314b503b4e4516a5802ca8f
kimhongjung <hong267@naver.com>
2022-04-30 13:29:05 +0900

    add programmers 0430_1

--- programmers/순위.py
+++ programmers/순위.py
@@ -1,81 +1,35 @@
-def solution(n, results): 
+from collections import defaultdict
+
+def solution(n, results):
+    def dfs(a):
+        nonlocal matches, results_dict
+
+        stack = [a]
+        while stack:
+            winner = stack.pop()
+            for loser in results_dict[winner]:
+                if matches[a][loser] == 0:
+                    matches[a][loser] = 1
+                    matches[loser][a] = 2
+                    stack.append(loser)
+
     answer = 0
+    matches = [[0] * (n + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        matches[i][i] = 3
+
+    results_dict = defaultdict(list)
+    for a, b in results:
+        results_dict[a].append(b)
+    
+    for i in range(1, n + 1):
+        dfs(i)
+
+    answer = 0
+    for i in range(1, n + 1):
+        if 0 not in matches[i][1:]:
+            answer += 1
+
     return answer
 
 print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))
-
-
-# from collections import defaultdict
-
-# def solution(n, results):
-
-#     def dfs(i):
-#         nonlocal n, results, results_dict, answer_list, check
-        
-#         stack = [[i, [i]]]
-#         while stack:
-#             a, trace = stack.pop()
-#             check[a] += 1
-#             if results_dict[a] == []:
-#                 answer_list.append(trace)
-#             else:
-#                 for j in results_dict[a]:
-#                     stack.append([j, trace + [j]])
-
-
-#     results_dict = defaultdict(list)
-#     for a, b in results:
-#         results_dict[a].append(b)
-    
-#     check = [0] * (n + 1)
-#     answer_list = []
-#     for i in list(results_dict.keys()):
-#         if results_dict[i] != []:
-#             dfs(i)
-            
-#     answer = 0
-#     length = len(answer_list)
-#     for c in check:
-#         if c == length:
-#             answer += 1
-
-#     return answer
-
-# print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))
-
-
-# from collections import defaultdict
-
-# def solution(n, results):
-
-#     def dfs(i, trace):
-#         nonlocal n, results, results_dict, answer_list, check
-
-#         check[i] += 1
-#         if results_dict[i] == []:
-#             answer_list.append(trace)
-#             return
-
-#         for j in results_dict[i]:
-#             dfs(j, trace + [j])
-
-
-#     results_dict = defaultdict(list)
-#     for a, b in results:
-#         results_dict[a].append(b)
-
-#     answer_list = []
-#     check = [0] * (n + 1)
-#     for i in range(1, n + 1):
-#         if results_dict[i] == []:
-#             continue
-#         dfs(i, [i])
-    
-#     answer = 0
-#     length = len(answer_list)
-#     for c in check:
-#         if c == length:
-#             answer += 1
-#     return answer
-
-# print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))
