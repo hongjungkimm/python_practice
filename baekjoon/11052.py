c564d062dac0300bbabfc9073ce924929139f02a
kimhongjung <hong267@naver.com>
2022-05-24 23:14:20 +0900

    add boj 0524_2

--- baekjoon/11052.py
+++ baekjoon/11052.py
@@ -1,26 +1,38 @@
 import sys
-from collections import defaultdict
-
-def find_max(cnt, amount):
-    global N, cards_dict, max_amount
-
-    if cnt == 0:
-        if amount > max_amount:
-            max_amount = amount
-        return
-    
-    if cnt > 0:
-        for i in range(1, cnt + 1):
-            find_max(cnt - i, amount + cards_dict[i])
-
 
 N = int(sys.stdin.readline())
-cards_list = list(map(int, sys.stdin.readline().split()))
-cards_dict = defaultdict(int)
-for i in range(N):
-    cards_dict[i+1] = cards_list[i]
+cards_list =  [0] + list(map(int, sys.stdin.readline().split()))
+dp = [0 for _ in range(N + 1)]
 
-max_amount = 0
-find_max(N, 0)
+for i in range(1, N + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i-j] + cards_list[j])
 
-print(max_amount)
+print(dp[N])
+
+# import sys
+# from collections import defaultdict
+
+# def find_max(cnt, amount):
+#     global N, cards_dict, max_amount
+
+#     if cnt == 0:
+#         if amount > max_amount:
+#             max_amount = amount
+#         return
+    
+#     if cnt > 0:
+#         for i in range(1, cnt + 1):
+#             find_max(cnt - i, amount + cards_dict[i])
+
+
+# N = int(sys.stdin.readline())
+# cards_list = list(map(int, sys.stdin.readline().split()))
+# cards_dict = defaultdict(int)
+# for i in range(N):
+#     cards_dict[i+1] = cards_list[i]
+
+# max_amount = 0
+# find_max(N, 0)
+
+# print(max_amount)
